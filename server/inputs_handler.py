ceaaf8e25a81f851a8da79eef7c44f4344058e3c
thomashebrard <thomas@pipelex.com>
2025-11-14 16:16:08 +0100

    make cc

--- server/inputs_handler.py
+++ server/inputs_handler.py
@@ -1,9 +1,12 @@
 from typing import Any
+
 from pipelex.core.stuffs.structured_content import StructuredContent
+
 
 class DictStuff(StructuredContent):
     concept: str
     content: Any
+
 
 class PipelineInputs(StructuredContent):
     inputs: dict[str, DictStuff]
